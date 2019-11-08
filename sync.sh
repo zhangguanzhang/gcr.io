9310c5eceac2d73bb221a4e3faa7c68024b1e213
zhangguanzhang <zhangguanzhang@qq.com>
2019-11-08 15:23:55 +0000

    use gcloud docker image

--- sync.sh
+++ sync.sh
@@ -55,16 +55,16 @@ Multi_process_init() {
 git_commit(){
     local COMMIT_FILES_COUNT=$(git status -s|wc -l)
     local TODAY=$(date +%F)
-    mkdir docker
-    cp -a /tmp/docker/* docker/
-    cat>Dockerfile<<-EOF
-        FROM zhangguanzhang/alpine
-        COPY docker/* /root/
-EOF
-    rm -rf docker
-    docker build -t status_image_name .
-    docker push status_image_name
     if [[ $COMMIT_FILES_COUNT -ne 0 && $(( (`date +%s` - start_time)/60 ))  -gt $push_time ]];then
+        mkdir docker
+        cp -a /tmp/docker/* docker/
+        cat>Dockerfile<<-EOF
+            FROM zhangguanzhang/alpine
+            COPY docker/* /root/
+EOF
+        rm -rf docker
+        docker build -t status_image_name .
+        docker push status_image_name
         git add -A
         git commit -m "Synchronizing completion at $TODAY"
         git push -u origin develop
@@ -83,6 +83,7 @@ gcloud_auth(){
       gcr.io/google.com/cloudsdktool/cloud-sdk:latest gcloud auth activate-service-account --key-file=/root/gcloud.config.json ||
         echo "gcloud service account is exsits"
 }
+
 
 #  GCR_IMAGE_NAME  tag  REPO_IMAGE_NAME
 image_tag(){
