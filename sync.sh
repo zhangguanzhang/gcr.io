8755efd8abeea401b088584d375f0d735f47eb4e
zhangguanzhang <zhangguanzhang@qq.com>
2019-11-08 15:02:04 +0000

    use gcloud docker image

--- sync.sh
+++ sync.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 [ -n "$DEBUG" ] && set -x
+readonly CUR_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 MY_REPO=zhangguanzhang
+status_image_name=zhangguanzhang/gcr-data
 interval=.
 : ${max_process:=10} ${max_per:=70} ${push_time:=45}
-google_list=sync_list/google.loop
-#--------------------------
 
-Multi_process_init() {
-    trap 'exec 5>&-;exec 5<&-;exit 0' 2
-    pipe=`mktemp -u tmp.XXXX`
-    mkfifo $pipe
-    exec 5<>$pipe
-    rm -f $pipe
-    seq $1 >&5
-}
+# 
 
+google_list=/tmp/docker/google.loop
+hub_check_time=/tmp/docker/hub_check.time
+hub_check_ns=/tmp/docker/hub_check.ns
+hub_check_name=/tmp/docker/hub_check.name
+    
+[ -z "$start_time" ] && start_time=$(date +%s)
 git_init(){
     git config --global user.name "zhangguanzhang"
     git config --global user.email zhangguanzhang@qq.com
@@ -32,59 +31,56 @@ git_init(){
         git pull origin develop
     fi
 }
+git_init
+
+mkdir -p /tmp/docker
+docker run -d --rm --name data $status_image_name sleep 10
+while read file;do
+    docker cp data:/root/$file /tmp/docker/
+done < <(docker exec name ls /root/)
+
+#--------------------------
+
+Multi_process_init() {
+    trap 'exec 5>&-;exec 5<&-;exit 0' 2
+    pipe=`mktemp -u tmp.XXXX`
+    mkfifo $pipe
+    exec 5<>$pipe
+    rm -f $pipe
+    seq $1 >&5
+}
+
+
 
 git_commit(){
-     local COMMIT_FILES_COUNT=$(git status -s|wc -l)
-     local TODAY=$(date +%F)
-     if [[ $COMMIT_FILES_COUNT -ne 0 && $(( (`date +%s` - start_time)/60 ))  -gt $push_time ]];then
+    local COMMIT_FILES_COUNT=$(git status -s|wc -l)
+    local TODAY=$(date +%F)
+    mkdir docker
+    cp -a /tmp/docker/* docker/
+    cat>Dockerfile<<-EOF
+        FROM zhangguanzhang/alpine
+        COPY docker/* /root/
+EOF
+    rm -rf docker
+    docker build -t status_image_name .
+    docker push status_image_name
+    if [[ $COMMIT_FILES_COUNT -ne 0 && $(( (`date +%s` - start_time)/60 ))  -gt $push_time ]];then
         git add -A
         git commit -m "Synchronizing completion at $TODAY"
         git push -u origin develop
-     fi
-}
-
-add_yum_repo() {
-cat > /etc/yum.repos.d/google-cloud-sdk.repo <<EOF
-[google-cloud-sdk]
-name=Google Cloud SDK
-baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-}
-
-add_apt_source(){
-    export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-    echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-}
-
-install_sdk() {
-    local OS_VERSION=$(grep -Po '(?<=^ID=")\w+' /etc/os-release)
-    local OS_VERSION=${OS_VERSION:-ubuntu}
-    if [[ $OS_VERSION =~ "centos" ]];then
-        if ! [ -f /etc/yum.repos.d/google-cloud-sdk.repo ];then
-            add_yum_repo
-            yum -y install google-cloud-sdk
-        else
-            echo "gcloud is installed"
-        fi
-    elif [[ $OS_VERSION =~ "ubuntu" ]];then
-        if ! [ -f /etc/apt/sources.list.d/google-cloud-sdk.list ];then
-            add_apt_source
-            sudo apt-get -y update && sudo apt-get -y install google-cloud-sdk
-        else
-             echo "gcloud is installed"
-        fi
     fi
 }
 
-auth_sdk(){
-    local AUTH_COUNT=$(gcloud auth list --format="get(account)"|wc -l)
-    [ "$AUTH_COUNT" -eq 0 ] && gcloud auth activate-service-account --key-file=$HOME/gcloud.config.json ||
+
+gcloud(){
+    docker run --rm -v /tmp/gcloud:/root/.config/gcloud  gcr.io/google.com/cloudsdktool/cloud-sdk:latest gcloud "$@"
+}
+
+gcloud_auth(){
+    mkdir /tmp/gcloud
+    docker run --rm -v /tmp/gcloud:/root/.config/gcloud \
+      -v $PWD/gcloud.config.json:/root/gcloud.config.json \
+      gcr.io/google.com/cloudsdktool/cloud-sdk:latest gcloud auth activate-service-account --key-file=$HOME/gcloud.config.json ||
         echo "gcloud service account is exsits"
 }
 
@@ -95,6 +91,7 @@ image_tag(){
     docker tag $1:$2 $3:$2
     docker rmi $1:$2
 }
+
 
 img_clean(){
     local domain=$1 namespace=$2 image_name=$3
@@ -109,33 +106,30 @@ img_clean(){
     git_commit
 }
 
-# google::name(){
-#     gcloud container images list --repository=$@ --format="value(NAME)"
-# }
-# google::tag(){
-#     gcloud container images list-tags $@  --format="get(TAGS)" --filter='tags:*' | sed 's#;#\n#g'
-# }
-# google::latest::digest(){
-#     gcloud container images list-tags --format='get(DIGEST)' $@ --filter="tags=latest"
-# }
 
 # example: gcr.io/$ns  return gcr.io/$ns/$img_name
 google::name(){
-    curl -XPOST -ks 'https://console.cloud.google.com/m/gcr/entities/list' \
-           -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.7 Safari/537.36' \
-           -H 'Content-Type: application/json;charset=UTF-8' \
-           -H 'Accept: application/json, text/plain, */*' \
-           --data-binary ['"'"${@#*/}"'"']   |
-        awk -vio=$@ -F'"' '/"/{if(NR==3){if(!a[$4]++)print io"/"$4}else{if(!a[$2]++)print io"/"$2}}'
+    gcloud container images list --format="value(NAME)" --repository=$@
 }
 # example: gcr.io/$ns/$img_name return $tag
 google::tag(){
-    gcloud container images list-tags $@  --format="get(TAGS)" --filter='tags:*' | sed 's#;#\n#g'
+    gcloud container images list-tags  --format="get(TAGS)" --filter='tags:*' $@  | sed 's#;#\n#g'
 }
 # example: gcr.io/$ns/$img_name return $sha256
 google::latest_digest(){
-    gcloud container images list-tags --format='get(DIGEST)' $@ --filter="tags=latest"
+    gcloud container images list-tags --format='get(DIGEST)' --filter="tags=latest" $@ 
 }
+
+
+# google::name(){
+#     https://console.cloud.google.com/gcr/images/google-containers/GLOBAL
+#     curl -XPOST -ks 'https://console.cloud.google.com/m/gcr/entities/list' \
+#            -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.7 Safari/537.36' \
+#            -H 'Content-Type: application/json;charset=UTF-8' \
+#            -H 'Accept: application/json, text/plain, */*' \
+#            --data-binary ['"'"${@#*/}"'"']   |
+#         awk -vio=$@ -F'"' '/"/{if(NR==3){if(!a[$4]++)print io"/"$4}else{if(!a[$2]++)print io"/"$2}}'
+# }
 # google::tag(){
 #     read null ns name< <(tr '/' ' '<<<$@)
 #     curl -ks -XGET https://gcr.io/v2/${ns}/${name}/tags/list | jq -r .tags[]
@@ -183,16 +177,13 @@ image_pull(){
             }
             [ -f "$domain/$namespace/$image_name/$tag" ] && { trvis_live;continue; }
             [[ $(df -h| awk  '$NF=="/"{print +$5}') -ge "$max_per" || -n $(sync_commit_check) ]] && { wait;img_clean $domain $namespace $image_name $@::latest_digest; }
-#             [[ "$image_name" =~ "$big_tag" ]] && {
-#                 image_tag $SYNC_IMAGE_NAME $tag $MY_REPO/$MY_REPO_IMAGE_NAME
-#                 img_clean $domain $namespace $image_name $@::latest_digest
-#             } || {
+
                 read -u5
                 {
                     [ -n "$tag" ] && image_tag $SYNC_IMAGE_NAME $tag $MY_REPO/$MY_REPO_IMAGE_NAME
                     echo >&5
                 }&
-#             }
+
         done < <($@::tag $SYNC_IMAGE_NAME)
         wait
         img_clean $domain $namespace $image_name $@::latest_digest
@@ -231,49 +222,70 @@ sync_domain_repo(){
 }
 
 
+
 main(){
-    
-    [ -z "$start_time" ] && start_time=$(date +%s)
-    git_init
+
     Multi_process_init $(( max_process * 4 ))
     live_start_time=$(date +%s)
-    read sync_time < sync_check
-    [ $(( (`date +%s` - sync_time)/3600 )) -ge 6 ] && {
-        [ ! -f sync_list_ns ] && ls gcr.io > sync_list_ns
-        allns=(`xargs -n1 < sync_list_ns`)
+    read sync_time < $hub_check_time #每隔 12个小时以dockerhub为准，清楚本地多余的文件
 
-        for ns in ${allns[@]};do 
-            [ ! -f sync_list_name ] && ls gcr.io/$ns > sync_list_name
-            allname=(`xargs -n1 < sync_list_name`)
-            for name in ${allname[@]};do
+    [ $(( (`date +%s` - sync_time)/3600 )) -ge 12 ] && {
+        [ ! -f "$hub_check_ns" ] && ls gcr.io > "$hub_check_ns"
+        hub_ns_break_count=`wc -l $hub_check_time | cut -d " " -f1`
+        i=0
+        while read ns;do
+            [ "$i" -eq "$hub_ns_break_count" ] && break #防止循环,----之间才是loop核心
+            #-------------
+            [ ! -f "$hub_check_name" ] && ls gcr.io/$ns > $hub_check_name
+            hub_name_break_count=`wc -l $hub_check_time | cut -d " " -f1`
+            j=0
+            while read name;do
+                [ "$j" -eq "$hub_name_break_count" ] && break #防止循环,----之间才是loop核心
+                #-------------
                 sync_domain_repo gcr.io/$ns/$name
-                perl  -i -lne 'print if $_ ne "'$name'"' sync_list_name
-            done
-            rm -f sync_list_name
-            perl  -i -lne 'print if $_ ne "'$ns'"' sync_list_ns
-        done
-        rm -f sync_list_ns
+                #-------------
+                sed -i 1d "$hub_check_name"
+                let j++
+            done < "$hub_check_name"
+            rm -f "$hub_check_name"
+            #-------------
+            sed -i 1d "$hub_check_ns"
+            let i++
+        done < "$hub_check_ns"
+        rm -f "$hub_check_ns"
         echo the sync has done! 
-        date +%s > sync_check
-        git_commit
+        date +%s > $hub_check_time
     }
     exec 5>&-;exec 5<&-
-
-    install_sdk
-    auth_sdk
+    #开始同步gcr
+    gcloud_auth
     Multi_process_init $max_process
 
-    GOOLE_NAMESPACE=(`grep -v '#' $google_list | xargs -n1 `)
-    for repo in ${GOOLE_NAMESPACE[@]};do
+    list_loop_break_count=`wc -l $google_list | cut -d " " -f1`
+    i=0
+    while read repo;do
+        [ "$i" -eq "$list_loop_break_count" ] && break
         image_pull gcr.io/$repo google
-        sed -i '/'"$repo"'/d' $google_list;echo "$repo" >> $google_list
-    done
+        sed -i 1d txt
+        echo $repo >> "$google_list"
+        let i++
+    done < "$google_list"
 
     exec 5>&-;exec 5<&-
     
     COMMIT_FILES_COUNT=$(git status -s|wc -l)
     TODAY=$(date +%F)
+    mkdir docker
+    cp -a /tmp/docker/* docker/
+    cat>Dockerfile<<-EOF
+        FROM zhangguanzhang/alpine
+        COPY docker/* /root/
+EOF
+    rm -rf docker
+    docker build -t status_image_name .
+    docker push status_image_name
     if [ $COMMIT_FILES_COUNT -ne 0 ];then
+        rm -rf docker
         git add -A
         git commit -m "Synchronizing completion at $TODAY"
         git push -u origin develop
@@ -281,3 +293,4 @@ main(){
 }
 
 main
+
